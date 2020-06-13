d8eb9798eefc0339b3dc39b12beb40b450be04f1
Pratik Kadam <pratik.kadam2601@gmail.com>
2020-06-13 12:23:29 +0530

    Add files via upload

---
+++ scripts/print_names.py
@@ -0,0 +1,5 @@
+f=open("name.txt")
+data=f.readlines()
+for line in data:
+    print(line)
+f.close()
---
+++ scripts/print_numbers.py
@@ -0,0 +1,6 @@
+import time
+num=0
+while True:
+	print 'Current count is :',num
+	num+=1
+	time.sleep(1)
